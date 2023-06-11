0d017bc1e5de9b6fd49572f742f0efa9473ca9cb
tertius Jubber <tjubber@gmail.com>
2023-06-11 17:43:19 +0200

    "encapsultion and abstraction"

--- objects-espresso-machine/js/main.js
+++ objects-espresso-machine/js/main.js
@@ -1,1 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine {
+  constructor(color,make,model,price) {  //adding properties & methods = Encapsalation
+    this.color = color                   //
+    this.make = make                     //
+    let model = model    // <============= making it private = abstraction
+    this.price = price
+  }
+  turnOn(){
+  console.log("im on")
+  }
+  steam(){
+  console.log("its getting hot")
+  }
+  brew(){
+  console.log("getting order ready")
+  }
+}
+
+let gaggia = new EspressoMachine("red","gagia","classic pro",500)
